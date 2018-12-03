14c5bd20d42b641594bc3192727bbda2ab04f4f8
boker123 <baiziyu123456789@163.com>
2018-12-03 23:47:12 +0800

    debug fix image show in one page

--- scripts/showPic.js
+++ scripts/showPic.js
@@ -1,5 +1,6 @@
+// 改变图片样式
 function showPic(whichpic) {
     var source = whichpic.getAttribute("href");
-    var placeholder = document.getElementById("placehoder");
+    var placeholder = document.getElementById("placeholder");
     placeholder.setAttribute("src",source);
 }
