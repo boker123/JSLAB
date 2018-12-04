d3cf9a1b5d1aa0674a37e69c07c7f7120f9f828a
boker123 <baiziyu123456789@163.com>
2018-12-04 17:21:55 +0800

    add css

--- scripts/showPic.js
+++ scripts/showPic.js
@@ -1,6 +1,9 @@
 // 改变图片样式
 function showPic(whichpic) {
     var source = whichpic.getAttribute("href");
+    var text = whichpic.getAttribute("title");
+    var description = document.getElementById("description");
     var placeholder = document.getElementById("placeholder");
     placeholder.setAttribute("src",source);
+    description.firstChild.nodeValue = text;
 }
