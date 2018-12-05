2b13b423971aa557a3a86f2902ee7f22a732d36b
boker123 <baiziyu123456789@163.com>
2018-12-05 23:44:07 +0800

    平稳退化

--- scripts/showPic.js
+++ scripts/showPic.js
@@ -1,4 +1,4 @@
-// 改变图片样式
+// 改变显示图片的方式让他在同一页面加载
 function showPic(whichpic) {
     var source = whichpic.getAttribute("href");
     var text = whichpic.getAttribute("title");
@@ -6,4 +6,25 @@ function showPic(whichpic) {
     var placeholder = document.getElementById("placeholder");
     placeholder.setAttribute("src",source);
     description.firstChild.nodeValue = text;
+}
+
+//分离js和html代码，并设置平稳退化
+function prepareGallery() {
+    //测试浏览器支持和检查选择器是否存在
+    if(!document.getElementsByTagName) 
+        return false;
+    if(!document.getElementById)
+        return false;
+    if(!document.getElementById("imagegallery"))
+        return false;
+    //创建基本变量
+    var gallery = document.getElementById("imagegallery");
+    var links = gallery.getElementsByTagName("a");
+    //为每一个图片添加onclik事件
+    for(var i = 0; i < links.length; i++){
+        links[i].onclick = function() {
+            showPic(this);
+            return false;
+        }
+    }
 }
